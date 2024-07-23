3859ef49bdf1eb873cc520cb363b0b1fa656afc9
Matthew Mahan <themaskedkid321@gmail.com>
2024-07-23 15:12:35 -0500

    more specific init

--- src/data.rs
+++ src/data.rs
@@ -1,7 +1,14 @@
-use std::fs;
+use std::{fs, io::Error};
 
 const GIT_DIR: &str = ".ritz";
 
 pub fn init() {
-    fs::create_dir_all(GIT_DIR);
+    if (std::path::Path::new(GIT_DIR).exists()) {
+        println!("Already a ritz repository.");
+        return;
+    }
+    match fs::create_dir_all(GIT_DIR) {
+        Ok(_) => println!("Successfully initialized ritz repository."),
+        Err(e) => println!("{:?}", e),
+    }
 }
